3e3842098eff180b4907dd3b6d3814d15bbce1e7
Josh Ackland <jackland@stotthoare.com.au>
2023-04-18 16:24:54 +0800

    Splitup APIs into Management and Storefront

--- reference_merge.py
+++ reference_merge.py
@@ -1,34 +1,45 @@
 import os
 import yaml  # pip install pyyaml
 
-base_yml = {
-    'openapi': '3.0.0',
-    'info': {
-        'title': 'BigCommerce API',
-        'version': '3.0.0',
-        'termsOfService': 'https://www.bigcommerce.com/terms',
-        'description': 'BigCommerce API',
-        'contact': {
-            'name': 'BigCommerce',
-            'url': 'https://www.bigcommerce.com',
-            'email': 'support@bigcommerce.com'
-        }
-    },
-    'servers': {
-        'url': 'https://api.bigcommerce.com/stores/{store_hash}/v3',
-        'variables': {
-            'store_hash': {
-                'default': 'store_hash',
-                'description': 'Permanent ID of the BigCommerce store.'
-            },
-            'description': 'BigCommerce API Gateway'
-        }
-    },
-    'security': {
-        'X-Auth-Token': []
-    },
-    'paths': {}
+
+def base_yml(url, title):
+    return {
+        'openapi': '3.0.0',
+        'info': {
+            'title': title,
+            'version': '3.0.0',
+            'termsOfService': 'https://www.bigcommerce.com/terms',
+            'description': title,
+            'contact': {
+                'name': 'BigCommerce',
+                'url': 'https://www.bigcommerce.com',
+                'email': 'support@bigcommerce.com'
+            }
+        },
+        'servers': {
+            'url': url,
+            'variables': {
+                'store_hash': {
+                    'default': 'store_hash',
+                    'description': 'Permanent ID of the BigCommerce store.'
+                },
+                'description': 'BigCommerce API Gateway'
+            }
+        },
+        'security': {
+            'X-Auth-Token': []
+        },
+        'paths': {}
+    }
+
+
+ymls = {
+    'https://api.bigcommerce.com/stores/{store_hash}/v3': base_yml('https://api.bigcommerce.com/stores/{store_hash}/v3', 'BigCommerce API Management V3'),
+    'https://{store_domain}/api/storefront': base_yml('https://{store_domain}/api/storefront', 'BigCommerce API Storefront'),
+    'https://api.bigcommerce.com/stores/{store_hash}/v2': base_yml('https://api.bigcommerce.com/stores/{store_hash}/v2', 'BigCommerce API Management V2')
 }
+
+serverset = {}
 
 for file in os.listdir('./reference'):
     if not os.path.isfile(f'./reference/{file}'):
@@ -40,13 +51,19 @@ for file in os.listdir('./reference'):
     if not file.endswith('.yml') and not file.endswith('.yaml'):
         continue
 
-    with open(f'./reference/{file}', "r", encoding="utf8") as f:
+    with open(f'./reference/{file}', 'r', encoding='utf8') as f:
         print(file)
         f_yml = yaml.safe_load(f)
 
-        if 'paths' in f_yml:
-            for path, value in f_yml["paths"].items():
-                base_yml["paths"][path] = value
+        if 'servers' in f_yml and 'paths' in f_yml:
+            for server in f_yml['servers']:
+                url = server['url']
+                if server['url'] in ymls:
+                    for path, value in f_yml['paths'].items():
+                        ymls[server['url']]['paths'][path] = value
 
-with open('bigcommerce.yml', 'w', encoding='utf8') as outfile:
-    yaml.dump(base_yml, outfile, default_flow_style=False, allow_unicode=True)
+for collection in ymls.values():
+    if len(collection["paths"]) > 0:
+        with open(f'{collection["info"]["title"]}.yml', 'w', encoding='utf8') as outfile:
+            yaml.dump(collection, outfile, default_flow_style=False,
+                      allow_unicode=True)
